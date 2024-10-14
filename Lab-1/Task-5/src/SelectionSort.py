612cd66ab11f6578bd00bf89706b80a965abdeba
TumanovaNelly <2-man.off@mail.ru>
2024-10-14 10:45:37 +0300

    Renaming

--- Lab-1/Task-5/src/SelectionSort.py
+++
@@ -1,7 +0,0 @@
-def selection_sort(lst: list) -> None:
-    for start in range(len(lst) - 1):
-        mn = [lst[start], start]
-        for i in range(start + 1, len(lst)):
-            if lst[i] < mn[0]:
-                *mn, = lst[i], i
-        lst[start], lst[mn[1]] = lst[mn[1]], lst[start]
