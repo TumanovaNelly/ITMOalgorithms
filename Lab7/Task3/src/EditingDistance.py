74876286f68af77a8e27a9965d497a3e577e541c
TumanovaNelly <2-man.off@mail.ru>
2024-12-16 17:17:43 +0300

    lab7: EditingDistance++

--- Lab7/Task3/src/EditingDistance.py
+++ Lab7/Task3/src/EditingDistance.py
@@ -1,6 +1,42 @@
 from utils import read, write
 
 
+def editing_distance(word1: str, word2: str):
+    word1 = " " + word1
+    word2 = " " + word2
+    distance_data = [[0] * len(word1) for _ in range(len(word2))]
+    for i in range(len(word1)):
+        distance_data[0][i] = distance_data[i][0] = i
+
+    for line in range(1, len(word2)):
+        for column in range(1, len(word1)):
+            if word1[column] == word2[line]:
+                distance_data[line][column] = distance_data[line - 1][column - 1]
+            else:
+                distance_data[line][column] = 1 + min(distance_data[line][column - 1],
+                                                      distance_data[line - 1][column - 1],
+                                                      distance_data[line - 1][column])
+
+    actions = []
+    line = len(word2) - 1
+    column = len(word1) - 1
+    while line >= 0 and column >= 0:
+        if word1[column] != word2[line]:
+            if distance_data[line][column - 1] == distance_data[line][column] - 1:
+                actions.append(f"del {word1[column]}")
+                line += 1
+            elif distance_data[line - 1][column - 1] == distance_data[line][column] - 1:
+                actions.append(f"change {word1[column]} {word2[line]}")
+            elif distance_data[line - 1][column] == distance_data[line][column] - 1:
+                actions.append(f"add {word2[line]}")
+                column += 1
+        line -= 1
+        column -= 1
+    return reversed(actions)
+
+
+
+
 def editing_distance_memory_optimized(word1: str, word2: str) -> int:
     old_line_distance_data = [i for i in range(len(word1) + 1)]
     new_line_distance_data = [0] * (len(word1) + 1)
@@ -22,6 +58,7 @@ def editing_distance_memory_optimized(word1: str, word2: str) -> int:
 def main():
     (word1, ), (word2, ) = read(type_convert=str)
     write(editing_distance_memory_optimized(word1, word2))
+    print(*editing_distance(word1, word2), sep='\n')
 
 
 if __name__ == "__main__":
